91f6bff22c56ff0525cb62feeff7e76f5f3448c8
David A. Casas <ise.david.casas@gmail.com>
2021-10-17 08:28:30 -0500

    add array js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+const products = [
+    { name: 'Cera Woodsman', id: 1},
+    { name: 'Colonia para barba', id: 2},
+    { name: 'Cepillo para barba', id: 3},
+    { name: 'Cera Reuzel', id: 4},
+    { name: 'Peine para bigote', id: 5},
+    { name: 'Cuchilla', id: 6}
+]
+
+console.log( products );
